30ec1976e5feb4742795eae515498cbc4189dde9
Abdurrahman Maihula <waleedmaihula.com>
2025-02-24 00:40:12 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#print is used to output or display information
+#print("Welcome to SWE4207")
+#print("I love programming!")
+#print("Programming is fun!")
+name = "Abdurraman Maihula"
+# + is used to concatanate or join strings
+print("My name is "+name)
+print(name.upper())
+print(name.lower())
+print(len(name))
